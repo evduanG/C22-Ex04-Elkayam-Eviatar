f7adc8a4ae729f46629cb0caafcadc65e3cb3712
evduanG <eviatar.duany@gmail.com>
2022-08-31 11:05:58 +0300

    try to merge

--- Ex04.Menus.Test/Program.cs
+++ Ex04.Menus.Test/Program.cs
@@ -1,26 +1,32 @@
-﻿using System;
+﻿using Ex04.Menus.Delegates;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Ex04.Menus.Test;
 using DelegatesMainMenu = Ex04.Menus.Delegates.MainMenu;
 
-
-// using InterfacesMenu = Ex04.Menus.Interfaces;
 namespace Program
 {
     internal class Program : IMenuItemSelectedObserver
     {
- // master
+        // master
+        private const char k_SpacesChar = ' ';
+        private const string k_TitleMainMenu = "Deleates Main Menu";
+        private const string k_TitleSubMenueVersionAndSpaces = "Version And Spaces";
+        private const string k_TitleSubMenueDateTime = "Show Date/Time";
+        private const string k_WaitForAnyInpu = "Enter any input to continue";
+        private const string k_Version = "Version: 22.3.4.8650";
+        private const string k_AskForLineToCountSpaces = @"Please enter the desired line where you want to count the Spaces";
+        private const string k_StrFormtOfCountSpace = @"There is a {0} Space in the line :
+{1}";
+
         private MainMenu m_MainMenu1;
         private MainMenu m_MainMenu2;
 
         public Program()
         {
             m_MainMenu1 = createIterfaceMenu();
-
-            // m_MainMenu2 = createDelegateMenu();
         }
 
         public static void Main()
@@ -28,7 +34,12 @@ namespace Program
             Program testProgram = new Program();
 
             testProgram.m_MainMenu1.Show();
-            // testProgram.m_MainMenu2.Show();
+
+            bool isInitSecsucceeded = createDelegatesMainMenu(out DelegatesMainMenu io_MainMenuDelegates);
+            if (isInitSecsucceeded)
+            {
+                io_MainMenuDelegates.Show();
+            }
         }
 
         private MainMenu createIterfaceMenu()
@@ -45,52 +56,7 @@ namespace Program
             return mainMenu2;
         }
 
-        public static void CountSpaces()
-        {
-            Console.WriteLine("Please enter your sentence:");
-            string userSentence = Console.ReadLine();
-            int numOfSpaces = 0;
-
-            foreach(char c in userSentence)
-            {
-               if(c == ' ')
-                {
-                    numOfSpaces++;
-                }
-            }
-
-            Console.WriteLine("There are {0} spaces in your sentence", numOfSpaces);
-        }
-
-        public static void ShowVersion()
-        {
-            Console.WriteLine("Version: 22.3.4.8650");
-        }
-
-        public static void ShowTime()
-        {
-            Console.WriteLine("{0}:{1}", DateTime.Now.Hour, DateTime.Now.Minute);
-          // 
-        private const char k_SpacesChar = ' ';
-        private const string k_TitleMainMenu = "Deleates Main Menu";
-        private const string k_TitleSubMenueVersionAndSpaces = "Version And Spaces";
-        private const string k_TitleSubMenueDateTime = "Show Date/Time";
-        private const string k_WaitForAnyInpu = "Enter any input to continue";
-        private const string k_Version = "Version: 22.3.4.8650";
-        private const string k_AskForLineToCountSpaces = @"Please enter the desired line where you want to count the Spaces";
-        private const string k_StrFormtOfCountSpace = @"There is a {0} Space in the line :
-{1}";
-
-        public static void Main()
-        {
-            bool isInitSecsucceeded = initiationDelegatesMainMenu(out DelegatesMainMenu io_MainMenuDelegates);
-            if(isInitSecsucceeded)
-            {
-                io_MainMenuDelegates.Show();
-            }
-        }
-
-        private static bool initiationDelegatesMainMenu(out DelegatesMainMenu io_MainMenuDelegates)
+        private static bool createDelegatesMainMenu(out DelegatesMainMenu io_MainMenuDelegates)
         {
             bool initiationDelegates = false;
             io_MainMenuDelegates = new DelegatesMainMenu(k_TitleMainMenu);
@@ -112,12 +78,39 @@ namespace Program
 
                 initiationDelegates = true;
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 Console.WriteLine(ex.ToString());
             }
 
             return initiationDelegates;
+        }
+
+        public static void CountSpaces()
+        {
+            Console.WriteLine("Please enter your sentence:");
+            string userSentence = Console.ReadLine();
+            int numOfSpaces = 0;
+
+            foreach(char c in userSentence)
+            {
+               if(c == k_SpacesChar)
+                {
+                    numOfSpaces++;
+                }
+            }
+
+            Console.WriteLine("There are {0} spaces in your sentence", numOfSpaces);
+        }
+
+        public static void ShowVersion()
+        {
+            Console.WriteLine("Version: 22.3.4.8650");
+        }
+
+        public static void ShowTime()
+        {
+            Console.WriteLine("{0}:{1}", DateTime.Now.Hour, DateTime.Now.Minute);
         }
 
         public static void MenuItem_Select_Version(object sender)
@@ -128,9 +121,7 @@ namespace Program
 
         public static void MenuItem_Select_CountSpaces(object sender)
         {
-            string userInputToCount = getLineForCountSpaces();
-            int numOfSpaces = countSpacaes(userInputToCount, k_SpacesChar);
-            Console.WriteLine(string.Format(k_StrFormtOfCountSpace, numOfSpaces, userInputToCount));
+            CountSpaces();
             waitForAnyInput();
         }
 
@@ -138,21 +129,6 @@ namespace Program
         {
             Console.WriteLine(k_WaitForAnyInpu);
             Console.Read();
-        }
-
-        private static int countSpacaes(string i_StrToCount, char i_CharToCount)
-        {
-            int cunt = 0;
-            char[] charsOfStr = i_StrToCount.ToCharArray();
-            foreach(char c in charsOfStr)
-            {
-                if(c == i_CharToCount)
-                {
-                    ++cunt;
-                }
-            }
-
-            return cunt;
         }
 
         private static string getLineForCountSpaces()
@@ -163,35 +139,15 @@ namespace Program
 
         public static void MenuItem_Select_ShowDate(object sender)
         {
-            showDateTime(eDateTime.ShowDate);
+            ShowDate();
             waitForAnyInput();
         }
 
         public static void MenuItem_Select_ShowTime(object sender)
         {
-            showDateTime(eDateTime.ShowTime);
             waitForAnyInput();
         }
 
-        private static void showDateTime(eDateTime i_DateTime)
-        {
-            string strToPrint;
-            switch (i_DateTime)
-            {
-                case eDateTime.ShowTime:
-                    strToPrint = DateTime.UtcNow.ToString();
-                    break;
-                case eDateTime.ShowDate:
-                    strToPrint = DateTime.Today.ToString();
-                    break;
-                default:
-                    strToPrint = DateTime.Now.ToString();
-                    break;
-            }
-
-            Console.WriteLine(strToPrint);
-     /// eviatar-dev
-        }
 
         public static void ShowDate()
         {
